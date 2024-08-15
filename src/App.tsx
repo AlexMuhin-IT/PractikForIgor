b7342f6e90fc3667dd074730f73dca6e1e8714e6
alexMuhin <myx87@bk.ru>
2024-08-15 12:02:13 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
-import {Body  }from './components/Body';
+import {Body} from './components/Body';
 
 function App() {
-  return (
-    <div className="App">
-      <Header title={'Header'}/>
-      <Body title={'Body'}/>
-      <Footer title={'Footer'}/>
-        <h2>Hello world</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header title={'Header'}/>
+            <Body title={'Body'}/>
+            <Footer title={'Footer'}/>
+            <h2>Hello world</h2>
+        </div>
+    );
 }
 
 export default App;
